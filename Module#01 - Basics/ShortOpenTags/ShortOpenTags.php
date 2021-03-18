85eaa9a3057578a6f4b21a212c7f24e200574017
João Lucas Requião <47596800+JLucasMelo@users.noreply.github.com>
2021-03-18 10:07:37 -0300

    Fix commit
    Fazendo o commit de alguns códigos que não foram colocados no primeiro commit

---
+++ Module#01 - Basics/ShortOpenTags/ShortOpenTags.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<head>
+    <title> PHP Basics 2 - utilizing short tags </title>
+    <meta charset="utf-8">
+</head>
+<body>
+    <?php
+
+    echo 'Utilizando a tag padrãod de declaração';
+
+    ?>
+    <br>
+    <?= 'Testando o uso da tag de impressão'?>
+    <br>
+    <? echo 'Testando utilização de tags curtas' ?>
+
+    <!--Modifying the php.ini file, them enabling the short_open_tag, (short_open_tag = "On") -->
+</body>
+
+<html>
